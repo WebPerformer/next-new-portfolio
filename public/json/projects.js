5bdaf634d41a6a3e7343aff0bb39a287ab290627
Gabriel Silva Araujo <110235876+WebPerformer@users.noreply.github.com>
2023-03-13 11:44:25 -0300

    Update projects.js

--- public/json/projects.js
+++ public/json/projects.js
@@ -6,45 +6,12 @@ export const data = {
         {
             "type": "web",
             "icon": <FaReact/>,
-            "image": "https://i.imgur.com/oqaRKoq.jpg",
-            "name": "My Personal Portfolio",
-            "link": "https://github.com/WebPerformer/next-new-portfolio",
-            "site": "https://next-new-portfolio.vercel.app/",
-            "categories": ["Next.js", "Greensock", "CSS", "Javascript"],
-            "date": "12/01/2023",
-            "credits": "Tutorial-Online.com"
-        },
-        {
-            "type": "dev",
-            "icon": <BiCodeAlt/>,
-            "image": "https://i.imgur.com/pKb8yxg.jpg",
-            "name": "Mousemove Parallax",
-            "link": "https://github.com/WebPerformer/next-mousemove-parallax",
-            "site": "https://next-mousemove-parallax.vercel.app/",
-            "categories": ["Next.js", "Vanilla.js", "CSS"],
-            "date": "13/02/2023",
-            "credits": "Tutorial-Online.com"
-        },
-        {
-            "type": "component",
-            "icon": <BiVector/>,
-            "image": "https://i.imgur.com/ed86VAX.jpg",
-            "name": "Tredy Scrolldown",
-            "link": "https://github.com/WebPerformer/next-scroll-effect-circular-text",
-            "site": "https://next-scroll-effect-circular-text.vercel.app/",
-            "categories": ["Next.js", "Jquery", "CSS"],
-            "date": "13/02/2023",
-            "credits": "Tutorial-Online.com"
-        },
-        {
-            "type": "component",
-            "icon": <BiVector/>,
-            "image": "https://i.imgur.com/9grKBUZ.jpg",
-            "name": "Custom Mouse Cursor",
-            "link": "https://github.com/WebPerformer/next-custom-mouse",
-            "site": "https://next-custom-mouse.vercel.app/",
-            "categories": ["Next.js", "Greensock", "CSS"],
-            "date": "14/02/2023",
+            "image": "https://i.imgur.com/4oPNEG7.jpg",
+            "name": "MERN Dashboard",
+            "link": "https://github.com/WebPerformer/MERN-Dashboard-Client",
+            "site": "https://mern-dashboard-client-omega.vercel.app/",
+            "categories": ["React.js", "MongoDB", "Express"],
+            "date": "06/03/2023",
             "credits": "Tutorial-Online.com"
         },
         {
@@ -59,14 +26,47 @@ export const data = {
             "credits": "Tutorial-Online.com"
         },
         {
+            "type": "component",
+            "icon": <BiVector/>,
+            "image": "https://i.imgur.com/9grKBUZ.jpg",
+            "name": "Custom Mouse Cursor",
+            "link": "https://github.com/WebPerformer/next-custom-mouse",
+            "site": "https://next-custom-mouse.vercel.app/",
+            "categories": ["Next.js", "Greensock", "CSS"],
+            "date": "14/02/2023",
+            "credits": "Tutorial-Online.com"
+        },
+        {
+            "type": "component",
+            "icon": <BiVector/>,
+            "image": "https://i.imgur.com/ed86VAX.jpg",
+            "name": "Tredy Scrolldown",
+            "link": "https://github.com/WebPerformer/next-scroll-effect-circular-text",
+            "site": "https://next-scroll-effect-circular-text.vercel.app/",
+            "categories": ["Next.js", "Jquery", "CSS"],
+            "date": "13/02/2023",
+            "credits": "Tutorial-Online.com"
+        },
+        {
+            "type": "dev",
+            "icon": <BiCodeAlt/>,
+            "image": "https://i.imgur.com/pKb8yxg.jpg",
+            "name": "Mousemove Parallax",
+            "link": "https://github.com/WebPerformer/next-mousemove-parallax",
+            "site": "https://next-mousemove-parallax.vercel.app/",
+            "categories": ["Next.js", "Vanilla.js", "CSS"],
+            "date": "13/02/2023",
+            "credits": "Tutorial-Online.com"
+        },
+        {
             "type": "web",
             "icon": <FaReact/>,
-            "image": "https://i.imgur.com/4oPNEG7.jpg",
-            "name": "MERN Dashboard",
-            "link": "https://github.com/WebPerformer/MERN-Dashboard-Client",
-            "site": "https://mern-dashboard-client-omega.vercel.app/",
-            "categories": ["React.js", "MongoDB", "Express"],
-            "date": "06/03/2023",
+            "image": "https://i.imgur.com/oqaRKoq.jpg",
+            "name": "My Personal Portfolio",
+            "link": "https://github.com/WebPerformer/next-new-portfolio",
+            "site": "https://next-new-portfolio.vercel.app/",
+            "categories": ["Next.js", "Greensock", "CSS", "Javascript"],
+            "date": "12/01/2023",
             "credits": "Tutorial-Online.com"
         },
     ]
